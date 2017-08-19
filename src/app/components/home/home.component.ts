5bd32aadca32860688cc6aa7ccd0a1292533f8dc
bohac-home <bohacc@seznam.cz>
2017-08-19 13:09:03 +0200

    add other components and rxjs events

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,9 +2,14 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.template.html'
+  templateUrl: 'home.template.html',
 })
 
 export class HomeComponent {
+  artists: string[] = [];
   constructor () {}
+
+  getArtists(data) {
+    this.artists.push(data);
+  }
 }
--- src/app/directives/autosearch/autosearch.ts
+++ src/app/directives/autosearch/autosearch.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, EventEmitter} from '@angular/core';
+import {Directive, ElementRef, EventEmitter, OnInit, Output} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/map';
@@ -6,11 +6,11 @@ import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/mergeAll';
 
 @Directive({
-  selector: '[appAutosearch]'
+  selector: '[autosearch]',
 })
 
-export class AutosearchDirective {
-  results: EventEmitter<Object[]> = new EventEmitter();
+export class AutosearchDirective implements OnInit {
+  @Output() results: EventEmitter<Object[]> = new EventEmitter();
   // service: Echonest; //Our service that queries the Echonest API and returns Observables
 
   constructor(private elementRef: ElementRef
@@ -19,12 +19,29 @@ export class AutosearchDirective {
     // this.service = service;
   }
 
+  ngOnInit() {
+    console.log('Observable.fromEvent 00');
+    this.onInit();
+  }
+
   onInit() {
+    console.log('Observable.fromEvent 0');
     Observable.fromEvent(this.elementRef.nativeElement, 'keyup')
-      .map(e => e['target'].value)
-      .filter(text => text.length > 2)
+      .map(e => {
+        // console.log(e);
+        return e['target'].value;
+      })
+      .filter(text => {
+        console.log(text);
+        return text.length > 2;
+      })
       // .map(name => this.service.artistSearch(name))
-      .mergeAll()
-      .subscribe(data => this.results.next(data['response']['artists']));
+      // .mergeAll()
+      .subscribe(data => {
+        console.log('Observable.fromEvent');
+        console.log(data);
+        this.results.next(data);
+        // this.results.next(data['response']['artists']);
+      });
   }
 }
